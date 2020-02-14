c7c7740b9c5f369c74e5af31016a0a3754ddcc79
psmax2008 <psmax2008@hotmail.com>
2020-02-14 21:13:26 +0800

    学生课程平均成绩和学科平均成绩

---
+++ ch1_1/c_point.cpp
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#define  STUDENT 5
+#define  SCORE 6
+
+void input_array(float(*score)[STUDENT]);
+void avg_score(float(*score)[STUDENT]);
+void avg_course(float(*score)[STUDENT]);
+
+int main()
+{
+	float a[SCORE][STUDENT];
+	input_array(a);
+	avg_course(a);
+	avg_score(a);
+
+}
+
+void input_array(float(*score)[STUDENT]) {
+	int i, j;
+	for (i = 0; i < SCORE; i++)
+	{
+		printf("input the %d student score:", i + 1);
+			for (j = 0; j < STUDENT; j++)
+			{
+				scanf("%f", score[i] + j);
+			}
+	}
+}
+
+void avg_course(float(*score)[STUDENT]) {
+	int i, j;
+	float s;
+			for (j=0;j<STUDENT;j++)
+			{
+				printf("course %d ", j);
+			}
+			printf("\n");
+			for (i=0;i<SCORE;i++)
+			{
+				s = 0;
+				for (j=0;j<STUDENT;j++)
+				{
+					printf("%f ", *(score[i] + j));
+					s += *(score[i] + j);
+
+				}
+				printf("\ts=%f,avg=%f\n", s, s / STUDENT);
+			}
+		}
+	
+void avg_score(float(*score)[STUDENT]) 
+{
+	int i, j;
+	float s;
+	for (i=0;i<STUDENT;i++)
+	{
+		s = 0;
+		for (j=0;j<SCORE;j++)
+		{
+			s += *(score[j] + i);
+		}
+		printf("%f ", s / SCORE);
+	}
+
+}
